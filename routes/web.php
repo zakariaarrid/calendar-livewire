c4fcdae8d97e71a13c6e0329e53ef101331086cb
zakariaarrid <zakariaarrid0@gmail.com>
2021-05-31 12:37:59 +0100

    Calendar livewire

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Livewire\Livewire;
+use App\Http\Livewire\Calendar;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::view('/', 'welcome');
+
+//Livewire::component('calendar', Calendar::class);
